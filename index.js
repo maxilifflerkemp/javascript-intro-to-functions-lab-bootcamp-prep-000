c3fbe9080374610f52341ef329a1b23924921ced
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-23 12:17:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ function logShout(string) { console.log(string.toUpperCase()) }
 function logWhisper(string) { console.log(string.toLowerCase()) }
 
 var lowercase = "hello!"
-
-if (lowercase.toLowerCase() === lowercase) { return ("I can't hear you!")}
-
 var uppercase = "HELLO!"
+var mixedCase = "I love you, Grandma."
+
+function sayHiToGrandma.if (lowercase.toLowerCase() === lowercase) { return ("I can't hear you!")}
 
 else if(uppercase.toUpperCase() === uppercase) { return ("YES INDEED!")}
-
-var mixedCase = "I love you, Grandma."
 
 else { return ("I love you, too.")}

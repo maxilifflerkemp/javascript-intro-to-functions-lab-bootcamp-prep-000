65934e744af310f11d8ae1bcdaa7deff0f06c196
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-23 12:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function whisper(string) {
 function logShout(string) { console.log(string.toUpperCase()) }
 
 function logWhisper(string) { console.log(string.toLowerCase()) }
+
+var uppercase = "HELLO!"
+
+uppercase.toUpperCase() === uppercase
+
+var lowercase = "hello!"
+
+lowercase.toLowerCase() === lowercase
+
+var mixedCase = "Hi there!"
+
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
+

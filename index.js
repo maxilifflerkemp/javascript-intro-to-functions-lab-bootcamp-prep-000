1c3d98a83027a4853eab0e311396364778fac1ac
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-23 12:28:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ var mixedCase = "Hi, there!"
 
 function sayHiToGrandma(string) {
 
-if(lowercase.toLowerCase() === lowercase) { return ("I can't hear you!")}
+if(lowercase.toLowerCase() === lowercase) { return "I can't hear you!" };
 
-else if(uppercase.toUpperCase() === uppercase) { return ("YES INDEED!")}
+else if(uppercase.toUpperCase() === uppercase) { return ("YES INDEED!") };
 
-else { return ("I love you, too.")}
+else { return ("I love you, too.") }
 }

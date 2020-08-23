cdc4a618a84acc23de21bd2b2b8a50e220d4b092
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-23 11:55:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) { console.log(string).toUpperCase }
+function logShout(string) { console.log(string).toUpperCase() }

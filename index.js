f451811b44544cff739350c94a1d5e9966312885
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-23 12:36:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function logShout(string) { console.log(string.toUpperCase()) }
 
 function logWhisper(string) { console.log(string.toLowerCase()) }
 
-function sayHiToGrandma(string) {
+function sayHiToGrandma(string) {(
 
-(if(string === string.toLowerCase) { return ("I can't hear you!") }
+if(string === string.toLowerCase) { return ("I can't hear you!") }
 
 else if(string === string.toUpperCase) { return ("YES INDEED!") }
 
